7a5d4e6551a5eacc2ae1d97087563949c218230f
jtpatrick1 <123102840+jtpatrick1@users.noreply.github.com>
2023-02-04 20:11:48 +0000

    Added the files for challenge 1 Completed the classExamples file.

---
+++ unit/unit01a/unit01a challenges/classExamples.cs
@@ -0,0 +1,32 @@
+//Use this file to store your work to demonstrate your understanding of operators
+//Make sure your work is different from the instructors examples or your peers
+using System;
+
+public class ClassExample
+{
+    
+}
+
+public class VariablesList
+{
+	public float weight = 1.5f;
+	public string hello = "Hello World";
+	public int number = 123;
+	public bool definitelyFalse = true;
+}
+
+public class Functions
+{
+	public int numberTwo = 12;
+    public void Main()
+    {
+        Console.WriteLine("cool things are happening");
+		Console.WriteLine(numberTwo);
+		moreVariables();
+    }
+    public void moreVariables()
+    {
+        int bobby = 30;
+        Console.WriteLine(bobby);
+    }
+}   
