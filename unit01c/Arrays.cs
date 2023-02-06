d53c78d4ee0cf73920059df731afa6d71c72bdd9
jtpatrick1 <123102840+jtpatrick1@users.noreply.github.com>
2023-02-06 04:48:24 +0000

    creates and adds code to Arrays

---
+++ unit01c/Arrays.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		int[] numberArray = {3, 4, 1, 6, 9};
+		Console.WriteLine(numberArray[1]);
+		
+		string[] videoGames = {"StarcraftII", "Halo", "Empires of the Undergrowth", "Call of Duty"};
+		Console.WriteLine(videoGames[numberArray[2]]);
+	}
+}
